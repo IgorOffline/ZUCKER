25ee90c45ebdf628d4e615ada6eefdb7f7b3f265
Igor Durbek <igor@igordurbek.com>
2025-05-30 17:16:34 +0200

    jooq select all countries

---
+++ ZuckerJooqOne/src/main/java/igoroffline/practice/zuckerjooqone/main/OneRead.java
@@ -0,0 +1,31 @@
+package igoroffline.practice.zuckerjooqone.main;
+
+import igoroffline.practice.zuckerjooqone.generated.public_.tables.Country;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class OneRead {
+
+    private final DSLContext dslContext;
+
+    public OneRead(DSLContext dslContext) {
+        this.dslContext = dslContext;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("--- INIT START ---");
+        final var countries = dslContext.selectFrom(Country.COUNTRY).fetch();
+        countries.forEach(country -> {
+            if (StringUtils.containsIgnoreCase(country.getCountry(), "germany")) {
+                log.info(country.getCountry());
+            }
+        });
+        log.info("--- INIT END ---");
+    }
+}
