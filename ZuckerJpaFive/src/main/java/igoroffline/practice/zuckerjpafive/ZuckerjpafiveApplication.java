362c3daa94c0b5b1b2baf1dcb6da88d72f8f09c9
Igor Durbek <igor@igordurbek.com>
2025-06-10 10:56:03 +0200

    Jpa prep

---
+++ ZuckerJpaFive/src/main/java/igoroffline/practice/zuckerjpafive/ZuckerjpafiveApplication.java
@@ -0,0 +1,13 @@
+package igoroffline.practice.zuckerjpafive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuckerjpafiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuckerjpafiveApplication.class, args);
+	}
+
+}
---
+++ ZuckerJpaFive/src/test/java/igoroffline/practice/zuckerjpafive/ZuckerjpafiveApplicationTests.java
@@ -0,0 +1,13 @@
+package igoroffline.practice.zuckerjpafive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuckerjpafiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
